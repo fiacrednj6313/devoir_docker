3bda187ad17d00c15a1a8043ac083404c517fb4e
fiacrednj <fiacrednj@gmail.com>
2024-08-12 10:07:53 +0300

    2m commit

---
+++ src/pages/InfoEtudiant.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../assets/css/InfoEtudiant.css'
+
+export default function InfoEtudiant() {
+  return (
+    <div className="info-box">
+        <div className="info">
+            <h1>Information etudiant</h1>
+            <p><span>Nom:</span> DAUPHIN</p>
+            <p><span>Prénom:</span> Nirina Jean Fiacre</p>
+            <p><span>Mention:</span> M2I</p>
+            <p><span>Niveau:</span> M2</p>
+            <p><span>Numéro d'inscription: </span> 3744</p>
+        </div>
+    </div>
+  )
+}
